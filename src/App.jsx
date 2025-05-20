ef3ef5c1fb4f55b539e6ea5de7cc9188e98a88e0
barunnbhattarai01 <barunnbhattarai@gmail.com>
2025-05-20 19:28:56 +0545

    new project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter } from "react-router-dom"
+import Navbar from "./component/navbar"
+
+
+function App() {
+ 
+
+  return (
+ <>
+ <BrowserRouter>
+ <Navbar/>
+ </BrowserRouter>
+ 
+ </>
+  )
+}
+
+export default App
