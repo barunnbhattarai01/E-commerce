efbfa9d16d2b5b190b86d45d32e9fb0f84683a48
barunnbhattarai01 <barunnbhattarai@gmail.com>
2025-05-22 16:23:40 +0545

    feature added

--- src/component/service.jsx
+++ src/component/service.jsx
@@ -3,11 +3,17 @@ import React from "react"
 function Services(){
 
 const[product,setProduct]=React.useState([]);
-const[buy,setbuy]=React.useState("");
-const[activeindex,setactiveIndex]=React.useState(null)
+const[buy,setbuy]=React.useState();
+const[activeindex,setactiveIndex]=React.useState(null); //it is used to open the div and close the div of name address and phone number
+const[close,setCLose]=React.useState("");
+
+
+
+
 
 function afterapply(index){
     setactiveIndex(index)// active index is produced when we clicked the button
+
 }
 
 
@@ -39,16 +45,17 @@ return(
     <p className=" font-bold text-3xl">{product.title}</p>
     <p className="text-2xl">PRICE:${product.price}</p>
     <p className="text-2xl">{product.description}</p>
-     <button className="ml-96 border border-amber-200 p-3 rounded-2xl hover:bg-amber-200" onClick={()=>afterapply(index)}>BUY</button>
+     <button className="ml-96 border border-amber-200 p-3 rounded-2xl hover:bg-amber-200" onClick={()=>afterapply(index)} >BUY</button>
 
  
- {activeindex===index && (
+ {activeindex===index &&  (
 <>
-<div className="h-32 flex flex-col">
-<span className="font-bold text-2xl">Name:<input type="text"/></span>
-<span className="font-bold text-2xl">Address:<input type="text"/></span>
-<span className="font-bold text-2xl">phone no:<input type="number"/></span>
-
+ <button className="ml-96 border border-amber-200 cursor-pointer" onClick={()=>setactiveIndex(null)}>❌</button>
+<div className="h-32 flex flex-col justify-center items-center ">
+<span className="font-bold text-2xl">Name:<input type="text" className="border border-black rounded-2xl mt-1"/></span>
+<span className="font-bold text-2xl">Address:<input type="text" className="border border-black rounded-2xl mt-1"/></span>
+<span className="font-bold text-2xl">phone no:<input type="number" className="border border-black rounded-2xl mt-1"/></span>
+<button className="border border-black rounded-2xl w-16 mx-auto mt-2 hover:bg-blue-300 ">Buy</button>
 </div>
 
 
@@ -79,3 +86,11 @@ return(
 
 
 export default Services
+
+
+//what i learn in this
+//active index is index which produced when we click the buttton 
+//index is index that was provided by map for product and it is unique
+//i learn to open form by conditional rendering and uisng active index and index in it
+//i learn to close the form by making setactiveindex null to close the form
+//to fetch the api 
