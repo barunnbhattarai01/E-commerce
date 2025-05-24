ed25af6df357bf8dde37b5106b3e3b73ab073e61
barunnbhattarai01 <barunnbhattarai@gmail.com>
2025-05-24 11:45:33 +0545

    css of product

--- src/component/product.jsx
+++ src/component/product.jsx
@@ -23,16 +23,33 @@ function Product(){
 
 return(
 <>
-<label>Search for items<input type="text" value={query} onChange={(e)=>setquery(e.target.value)} /></label>
+  <label className="text-white text-2xl flex items-center gap-4 bg-gray-800 px-4 py-2 rounded-lg shadow">
+    Search for items
+    <input
+      type="text"
+      className="text-white rounded px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-400 border border-white "
+      value={query}
+      onChange={(e) => setquery(e.target.value)}
+    />
+  </label>
 
-<div className="">
+<div className="flex flex-wrap gap-20 justify-center p-20">
 {
   filteredproduct.map(
   (pro,index)=>(
- <div className="" key={index}>
-   <img src={pro.image}/>
+    <>
+ <div className="overflow-x-hidden relative text-black bg-white p-6 rounded-xl shadow-lg border border-gray-200  w-[600px]  mt-7 dark:bg-black dark:text-white overflow-y-hidden max-h-500" key={index}>
+   <img src={pro.image} className="h-36"/>
    <p>{pro.title}</p>
+ <div className="mx-auto ">
+    <p className=" font-bold text-3xl">{pro.title}</p>
+    <p className="text-2xl">PRICE:${pro.price}</p>
+    <p className="text-2xl">{pro.description}</p>
+
+
     </div>
+    </div>
+    </>
 
   )
   
@@ -53,3 +70,5 @@ return(
 }
 
 export default Product;
+
+//in this section i learned to filter the array and then mapping all them 
