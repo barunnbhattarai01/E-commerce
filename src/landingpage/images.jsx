3a36f764c001d65e88b2951d88ef99d5528e1761
barunnbhattarai01 <barunnbhattarai@gmail.com>
2025-06-18 18:17:54 +0545

    darkmode added in photo section€

--- src/landingpage/images.jsx
+++ src/landingpage/images.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Photo() {
+  
+  const[theme,settheme]=React.useState(()=>{
+return localStorage.getItem("theme")
+
+  }
+
+  )
+
+
+
   const photos = [
   "one.jpg",
   "two.jpg",
@@ -9,12 +20,45 @@ function Photo() {
   "five.jpg"
   ];
 
+
+  //darkmode
+  React.useEffect(()=>{
+  if(theme==='dark'){
+    document.documentElement.classList.add("dark")
+  }
+  else{
+    document.documentElement.classList.remove("dark")
+  }
+},[theme])
+   //toggle light and dark mode
+
+   const toogle=()=>{
+    settheme(theme==="light"?"dark":"light");
+   }
+  
+
+    //navigate
+    const navigate=useNavigate();
+
+
   return (
-    <div className="h-screen w-auto bg-white">
-    <div className=" dark:border-gray-700  p-6 bg-white dark:bg-gray-900 transition-colors duration-300">
-      <h1 className="text-3xl font-bold text-center mb-6 text-gray-800 dark:text-white">
-        Images Gallery
-      </h1>
+  
+    <div className=" dark:border-gray-700  p-6 bg-white dark:bg-gray-900 transition-colors duration-300 md:h-screen md:w-auto h-auto" >
+       <div className="flex items-center justify-between gap-4 mb-6">
+  <h1 className="text-3xl font-bold text-gray-800 dark:text-white">
+   <button onClick={()=>navigate('/')}> Images Gallery</button>
+  </h1>
+  
+  <button 
+    onClick={toogle} 
+    className="text-2xl"
+    title="Toggle Theme"
+  >
+    {theme === "light" ? "🌑" : "☀️"}
+  </button>
+  
+</div>
+
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">
         {photos.map((src, index) => (
           <div
@@ -30,7 +74,7 @@ function Photo() {
         ))}
       </div>
     </div>
-    </div>
+
   );
 }
 
